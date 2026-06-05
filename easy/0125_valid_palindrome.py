fc81219f53b1e0e038a4bddd24f04fdc10c5f627
butialready <okanaksoy15@gmail.com>
2026-06-05 11:51:07 +0100

    Fix Valid Palindrome #125 - add solution

---
+++ easy/0125_valid_palindrome.py
@@ -0,0 +1,20 @@
+# 125. Valid Palindrome
+# https://leetcode.com/problems/valid-palindrome/
+# Difficulty: Easy
+# Pattern: Two Pointers
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+        return True
