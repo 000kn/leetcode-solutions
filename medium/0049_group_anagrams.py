1b4ae0a7d6c0199df61318c3d5eeb7b58d3c094c
butialready <okanaksoy15@gmail.com>
2026-06-06 13:59:06 +0100

    Add Group Anagrams #49 (Arrays & Hashing)

---
+++ medium/0049_group_anagrams.py
@@ -0,0 +1,18 @@
+# 49. Group Anagrams
+# https://leetcode.com/problems/group-anagrams/
+# Difficulty: Medium
+# Pattern: Arrays & Hashing
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26  # a ... z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
