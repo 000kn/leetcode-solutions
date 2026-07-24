541cc8dffe759732a31262fed4ffe6561e530b40
Okan <okanaksoy15@gmail.com>
2026-07-24 14:40:40 +0300

    Add Invert Binary Tree (Easy) - recursion, first Tree problem

---
+++ easy/0226_invert_binary_tree.py
@@ -0,0 +1,22 @@
+# 226. Invert Binary Tree
+# https://leetcode.com/problems/invert-binary-tree/
+# Difficulty: Easy
+# Pattern: Recursion (base case + swap children + recurse on subtrees)
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
