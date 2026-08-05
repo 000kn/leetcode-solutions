69835cc20e6bf47e5e3a4b4cba011887d5dea983
Okan <okanaksoy15@gmail.com>
2026-08-05 16:43:45 +0300

    Add Subsets (Medium) - backtracking include/exclude

---
+++ medium/0078_subsets.py
@@ -0,0 +1,23 @@
+# 78. Subsets
+# https://leetcode.com/problems/subsets/
+# Difficulty: Medium
+# Pattern: Backtracking - include/exclude decision tree
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        curr_subset = []
+
+        def backtrack(index):
+            if index == len(nums):
+                res.append(curr_subset[:])
+                return
+
+            curr_subset.append(nums[index])
+            backtrack(index + 1)
+
+            curr_subset.pop()
+            backtrack(index + 1)
+
+        backtrack(0)
+        return res
